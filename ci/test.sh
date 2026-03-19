241edcacc7f2843f80f8d368bfa10237ee7c3a52
tom-cruise10 <karaechi420@gmail.com>
2026-03-19 07:31:45 +0500

    CCC

--- ci/test.sh
+++ ci/test.sh
@@ -5,6 +5,7 @@ yarn --cwd ui build
 yarn --cwd ui test
 
 export GO111MODULE="on"
+go mod tidy
 go install github.com/rakyll/statik@latest
 go generate github.com/tom-cruise10/selenoid-ui
 go test -race -v -coverprofile=coverage.txt -covermode=atomic ./...
